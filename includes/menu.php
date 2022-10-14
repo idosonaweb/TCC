5187e8ab5d816b594cba6255823474d8fee102c5
idosonaweb <joao.piva@aluno.ifsp.edu.br>
2022-10-14 19:47:40 -0300

    commit

--- includes/menu.php
+++ includes/menu.php
@@ -1,67 +1,73 @@
-<div class="card">
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.104.2">
+    <title>Menu - Topo</title>
 
-    <div class="card-header">
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/headers/">
 
-        Menu
+<link href="css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
-    </div>
+<link rel="mask-icon" href="/docs/5.2/assets/img/favicons/safari-pinned-tab.svg" color="#712cf9">
 
-    <div class="card-body">
+    
+    <link rel="stylesheet" href="css/style.css">
 
-        <ul class="nav flex-column">
+    <link href="headers.css" rel="stylesheet">
+  </head>
+  <body>
 
-            <li class="nav-item">
+<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+  <symbol id="bootstrap" viewBox="0 0 118 94">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z"></path>
+  </symbol>
+</svg>
 
-                <a class="nav-link" href="index.php">Home</a>
+<main>
+  
+  <div>
+    
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+      
+        <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+        
+            <img class=" col-md-2 me-2" width="40" height="45" role="img" src="img/manager.png" aria-label="Bootstrap">
 
-            </li>
+            <span class="fs-4">Purchase Manager</span>
+      
+        </a>
 
-            <li class="nav-item">
+        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
 
-                <a class="nav-link" href="usuario_formulario.php">Cadastre-se</a>
-
-            </li>
-
-
-fazer esse sumir depois de logar 
-            <li class="nav-item">
-
-                <a class="nav-link" href="login_formulario.php">Login</a>
-
-            </li>
-
-            <?php if (isset($_SESSION['login'])) : ?>
-
-            <li class="nav-item">
-
-                <a class="nav-link" href="post_formulario.php">Compras e Listas</a>
-
-            </li>
-
-            <?php endif; ?>
-
-            <?php if (isset($_SESSION['login'])) : ?>
-
-            <li class="nav-item">
-
-                <a class="nav-link" href="post_formulario.php">Mercados</a>
-
-            </li>
-
-            <?php endif; ?>
-
-            <?php if (isset($_SESSION['login'])) : ?>
-
-            <li class="nav-item">
-
-                <a class="nav-link" href="post_formulario.php">Produtos</a>
-
-            </li>
-
-            <?php endif; ?>
-
+            <li><a href="#" class="nav-link text-dark px-3 ">Home</a></li>
+            
+            <li><a href="#" class="nav-link text-dark px-3 ">O que é</a></li>
+        
+            <li><a href="#" class="nav-link text-dark px-3 ">Funções</a></li>
+        
+            <li><a href="#" class="nav-link text-dark px-3 ">Como usar</a></li>
+        
+            <li><a href="#" class="nav-link text-dark px-3 ">Sou Mercado</a></li>
+        
         </ul>
 
-    </div>
-
+        <div class="col-md-3 text-end">
+        
+            <button type="button" class="btn btn-outline-primary me-2">Login</button>
+        
+            <button type="button" class="btn btn-outline-primary me-2">Cadastrar-se</button>
+      
+        </div>
+    
+    </header>
 </div>
+</main>
+
+    <script src="js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+      
+  </body>
+</html>
---
+++ index (2).php
@@ -0,0 +1,266 @@
+<!doctype html>
+
+<html lang="en">
+  
+    <head>
+
+        <meta charset="utf-8">
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <meta name="description" content="">
+        
+        <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+        
+        <meta name="generator" content="Hugo 0.104.2">
+        
+        <title>Purchase Manager</title>
+
+        <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/carousel/">
+
+        <link href="css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        
+        <link rel="manifest" href="/docs/5.2/assets/img/favicons/manifest.json">
+        
+        <meta name="theme-color" content="#712cf9">
+        
+        <link rel="stylesheet" href="css/style.css">
+
+        <link href="css/features.css" rel="stylesheet">
+
+        <link href="css/carousel.css" rel="stylesheet">
+
+    </head>
+
+  <body>
+
+  <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+    
+    <symbol id="geo-fill" viewBox="0 0 16 16">
+        
+        <path fill-rule="evenodd" d="M4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999zm2.493 8.574a.5.5 0 0 1-.411.575c-.712.118-1.28.295-1.655.493a1.319 1.319 0 0 0-.37.265.301.301 0 0 0-.057.09V14l.002.008a.147.147 0 0 0 .016.033.617.617 0 0 0 .145.15c.165.13.435.27.813.395.751.25 1.82.414 3.024.414s2.273-.163 3.024-.414c.378-.126.648-.265.813-.395a.619.619 0 0 0 .146-.15.148.148 0 0 0 .015-.033L12 14v-.004a.301.301 0 0 0-.057-.09 1.318 1.318 0 0 0-.37-.264c-.376-.198-.943-.375-1.655-.493a.5.5 0 1 1 .164-.986c.77.127 1.452.328 1.957.594C12.5 13 13 13.4 13 14c0 .426-.26.752-.544.977-.29.228-.68.413-1.116.558-.878.293-2.059.465-3.34.465-1.281 0-2.462-.172-3.34-.465-.436-.145-.826-.33-1.116-.558C3.26 14.752 3 14.426 3 14c0-.599.5-1 .961-1.243.505-.266 1.187-.467 1.957-.594a.5.5 0 0 1 .575.411z"/>
+    
+    </symbol>
+  
+  </svg>
+    
+<header>
+
+<main>
+
+  <div class="container marketing">
+
+    <hr id="Texto1" class="featurette-divider">
+
+    <div class="row featurette">
+      
+        <div class="col-md-7">
+        
+            <h2 class="featurette-heading fw-normal lh-1">Primeiro título de purchase. <span class="text-muted">Vai explodir sua mente.</span></h2>
+            
+            <p class="lead">Alguns ótimos conteúdos de espaço reservado para o primeiro featurette aqui. Imagine alguma prosa emocionante aqui.</p>
+    
+        </div>
+            
+        <div class="col-md-5">
+        
+            <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+
+        </div>
+    
+    </div>
+
+    <hr id="Texto2" class="featurette-divider">
+
+    <div class="row featurette">
+      
+        <div class="col-md-7 order-md-2">
+        
+            <h2 class="featurette-heading fw-normal lh-1">Ah sim, é tão bom. <span class="text-muted">Veja por si mesmo.</span></h2>
+            
+            <p class="lead">Outro recurso? É claro. Mais conteúdo de espaço reservado aqui para dar uma ideia de como esse layout funcionaria com algum conteúdo real do mundo real.</p>
+      
+        </div>
+      
+        <div class="col-md-5 order-md-1">
+        
+            <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+  
+        </div>
+    
+    </div>
+
+    <hr id="Texto3" class="featurette-divider">
+
+    <div class="row featurette">
+      
+        <div class="col-md-7">
+        
+            <h2 class="featurette-heading fw-normal lh-1">E por último, este. <span class="text-muted">Xeque-mate.</span></h2>
+            
+            <p class="lead">E sim, este é o último bloco de conteúdo representativo de espaço reservado. Novamente, não pretende ser realmente lido, apenas aqui para lhe dar uma visão melhor de como isso ficaria com algum conteúdo real. Seu conteúdo.</p>
+        
+        </div>
+        
+        <div class="col-md-5">
+        
+            <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+
+        </div>
+    
+    </div>
+
+    <br><br><br>
+
+    <div class="container px-3 py-5" id="custom-cards">
+    
+        <h2 class="pb-2 border-bottom">Integrantes</h2>
+
+        <div class="row row-cols-1 row-cols-lg-4 align-items-stretch g-5 py-5">
+        
+            <div class="col">
+        
+                <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" style="background-image: url('img/Breno.png');">
+                    
+                    <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+            
+                        <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</h3>
+            
+                            <ul class="d-flex list-unstyled mt-auto">
+                        
+                                <li class="me-auto">
+                                
+                                    <img src="img/purchase.png" alt="Bootstrap" width="32" height="32" class="rounded-circle border border-white">
+                                
+                                </li>
+                    
+                                <li class="d-flex align-items-center me-3">
+                            
+                                    <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#geo-fill"/></svg>
+                            
+                                    <small>Breno Campachi</small>
+                                
+                                </li>
+                                
+                            </ul>
+                    </div>
+                
+                </div>
+            
+            </div>
+
+    <div class="col">
+        
+        <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" style="background-image: url('img/Ellen.png');">
+          
+            <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+               
+                <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</h3>
+                
+                <ul class="d-flex list-unstyled mt-auto">
+                        
+                    <li class="me-auto">
+                    
+                        <img src="img/purchase.png" alt="Bootstrap" width="32" height="32" class="rounded-circle border border-white">
+                    
+                    </li>
+                        
+                    <li class="d-flex align-items-center me-3">
+                            
+                        <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#geo-fill"/></svg>
+                            
+                        <small>Ellen Nataly</small>
+                        
+                    </li>
+                        
+                </ul>
+                
+            </div>
+        
+        </div>
+      
+    </div>
+
+    <div class="col">
+            
+        <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" style="background-image: url('img/Pedro.png');">
+            
+            <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
+
+                <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</h3>
+        
+                <ul class="d-flex list-unstyled mt-auto">
+                
+                    <li class="me-auto">
+                    
+                        <img src="img/purchase.png" alt="Bootstrap" width="32" height="32" class="rounded-circle border border-white">
+                    </li>
+                            
+                    <li class="d-flex align-items-center me-4">
+                            
+                        <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#geo-fill"/></svg>
+                                
+                        <small>João Piva</small>
+                            
+                    </li>
+                </ul>
+            
+            </div>
+                
+        </div>
+            
+    </div>
+            
+    <div class="col">
+
+        <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" style="background-image: url('img/Pedro.png');">
+            
+            <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+
+                <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</h3> 
+                
+                <ul class="d-flex list-unstyled mt-auto">
+                        
+                    <li class="me-auto">
+                                    
+                        <img src="img/purchase.png" alt="Bootstrap" width="32" height="32" class="rounded-circle border border-white">
+                                
+                    </li>
+                                
+                    <li class="d-flex align-items-center me-7">
+                                    
+                        <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#geo-fill"/></svg>
+                                    
+                        <small>Pedro Arrieiro</small>
+                                
+                    </li>
+                
+                </ul>
+            
+            </div>
+        
+        </div>
+    
+    </div>
+
+  </div>
+
+</div>
+
+</div>
+
+  <footer class="container">
+    
+    <p class="float-end"><a class="btn btn-primary btn-lg" href="#">Voltar ao topo</a></p>
+    
+    <p>&copy; 2022 Company, Inc. All rights reserved.</p>
+    
+  </footer>
+
+    </main>
+
+    <script src="js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+      
+  </body>
+
+</html>
